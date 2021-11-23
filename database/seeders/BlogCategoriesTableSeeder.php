3dbb52270b64538eaed7efe1ddffed131d038072
yeontannie <tanastasuk35@gmail.com>
2021-11-23 13:46:41 +0200

    Lab2

---
+++ database/seeders/BlogCategoriesTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [];
+
+        $cName = 'Без категорії';
+        $categories[] = [
+            'title'     => $cName,
+            'slug'      => Str::slug($cName),
+            'parent_id' => 0,
+        ];
+
+        for ($i = 1; $i <=10; $i++) {
+            $cName = 'Категорія #'.$i;
+            $parentId = ($i > 4) ? rand(1, 4) : 1;
+
+            $categories[] = [
+                'title'     => $cName,
+                'slug'      => Str::slug($cName),
+                'parent_id' => $parentId,
+            ];
+        }
+
+        DB::table('blog_categories')->insert($categories);
+    }
+}
